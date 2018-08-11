523a7f4a5e82242c804f17e562eb65cbfdcc81f4
SaqibAltaf <saqibaltaf062@gmail.com>
2018-08-11 08:15:30 +0500

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TestApplication';
+export class AppComponent implements OnInit {
+  
+ngOnInit(){
+  $('li').removeClass();
+
+  $('.navbar-nav > li > a').click(function() {
+    $('li').removeClass();
+    $(this).parent().addClass('active');
+});
+
+$('.navbar-nav2 > li > a').click(function() {
+  $('li').removeClass();
+  $(this).parent().addClass('active2');
+});
+}
+ 
+
+
 }
